88f354f536cf2551fe34fbb2f1628cd6bd134f64
Vasiliy Sakharov <vasiliy.sakharov@gmail.com>
2020-07-11 20:48:49 +0300

    project created

---
+++ itsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itsApp
+//
+//  Created by Василий on 11/07/2020.
+//  Copyright © 2020 VS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
